389f8285a86f39afdee23bf3900ee5103ba510ee
MateusAranha <MatheusAranhaFranchini@gmail.com>
2023-06-15 17:03:29 -0300

    created fake api and added some styles

--- courses/2023/scale-react-development-with-nx/nxegghead/packages/store/src/app/app.tsx
+++ courses/2023/scale-react-development-with-nx/nxegghead/packages/store/src/app/app.tsx
@@ -1,51 +1,37 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+import { getAllGames } from '../fake-api';
 
-import { Route, Routes, Link } from 'react-router-dom';
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@material-ui/core';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="store" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
+    <div className={styles.container}>
+      <div className={styles.gamesLayout} >
+        {getAllGames().map((x) => (
+          <Card key={x.id} className={styles.gameCard}>
+            <CardActionArea>
+              <CardMedia
+                className={styles.gameCardMedia}
+                image={x.image}
+                title={x.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h2'>
+                  {x.name}
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>
+                  {x.description}
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component="p" className={styles.gameRating}>
+                  <strong>Rating:</strong> {x.rating}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
       </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
     </div>
   );
 }
