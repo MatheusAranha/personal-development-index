88f983f61b3fc9e9d75b405fadc3e4ea2b188478
MateusAranha <MatheusAranhaFranchini@gmail.com>
2023-06-16 11:07:04 -0300

    created a library with shared ui for the store project and a header component into the library

--- courses/2023/scale-react-development-with-nx/nxegghead/packages/store/src/app/app.tsx
+++ courses/2023/scale-react-development-with-nx/nxegghead/packages/store/src/app/app.tsx
@@ -3,36 +3,41 @@ import styles from './app.module.scss';
 
 import { getAllGames } from '../fake-api';
 
+import { Header } from 'store/ui-shared';
+
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@material-ui/core';
 
 export function App() {
   return (
-    <div className={styles.container}>
-      <div className={styles.gamesLayout} >
-        {getAllGames().map((x) => (
-          <Card key={x.id} className={styles.gameCard}>
-            <CardActionArea>
-              <CardMedia
-                className={styles.gameCardMedia}
-                image={x.image}
-                title={x.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant='h5' component='h2'>
-                  {x.name}
-                </Typography>
-                <Typography variant='body2' color='textSecondary' component='p'>
-                  {x.description}
-                </Typography>
-                <Typography variant='body2' color='textSecondary' component="p" className={styles.gameRating}>
-                  <strong>Rating:</strong> {x.rating}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.gamesLayout} >
+          {getAllGames().map((x) => (
+            <Card key={x.id} className={styles.gameCard}>
+              <CardActionArea>
+                <CardMedia
+                  className={styles.gameCardMedia}
+                  image={x.image}
+                  title={x.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='h2'>
+                    {x.name}
+                  </Typography>
+                  <Typography variant='body2' color='textSecondary' component='p'>
+                    {x.description}
+                  </Typography>
+                  <Typography variant='body2' color='textSecondary' component="p" className={styles.gameRating}>
+                    <strong>Rating:</strong> {x.rating}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
