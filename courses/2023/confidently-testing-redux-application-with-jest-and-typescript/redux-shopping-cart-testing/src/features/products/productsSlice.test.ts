6053d6cbfe021100dd41f59ef3d0f1b526e62252
MateusAranha <MatheusAranhaFranchini@gmail.com>
2023-07-03 16:00:21 -0300

    adjust: added more tests to the productsSlice

--- courses/2023/confidently-testing-redux-application-with-jest-and-typescript/redux-shopping-cart-testing/src/features/products/productsSlice.test.ts
+++ courses/2023/confidently-testing-redux-application-with-jest-and-typescript/redux-shopping-cart-testing/src/features/products/productsSlice.test.ts
@@ -2,6 +2,7 @@ import productsReducer, { receivedProducts } from "./productsSlice";
 import products from "../../../public/products.json";
 
 describe("products reducer", () => {
+
     it("should return the initial state when passed an empty action", () => {
         const initialState = undefined;
         const action = { type: "" };
@@ -9,6 +10,7 @@ describe("products reducer", () => {
 
         expect(result).toEqual({ products: {} });
     });
+
     it("should convert the products", () => {
         const initialState = undefined;
         const action = receivedProducts(products);
@@ -19,5 +21,34 @@ describe("products reducer", () => {
         products.forEach((product) => {
             expect(result.products[product.id]).toEqual(product);
         });
+    });
+
+    it("should not allow the same product to be added more than once", () => {
+        const initialState = undefined;
+        const action = receivedProducts(products);
+        let result = productsReducer(initialState, action);
+
+        expect(Object.keys(result.products).length).toEqual(products.length);
+
+        products.forEach((product) => {
+            expect(result.products[product.id]).toEqual(product);
+        });
+
+        result = productsReducer(result, action);
+
+        expect(Object.keys(result.products).length).toEqual(products.length);
+    });
+
+    it("should allow multiple products to be received at different times", () => {
+        const initialState = undefined;
+        const action = receivedProducts(products.slice(0, 2));
+        let result = productsReducer(initialState, action);
+
+        expect(Object.keys(result.products).length).toEqual(2);
+
+        const secondAction = receivedProducts(products.slice(2, 4));
+        result = productsReducer(result, secondAction);
+
+        expect(Object.keys(result.products).length).toEqual(4);
     });
 })
