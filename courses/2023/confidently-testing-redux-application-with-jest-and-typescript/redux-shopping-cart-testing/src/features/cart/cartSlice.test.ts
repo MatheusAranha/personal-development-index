c294f6f090ad962e99825a1d8843eb3c3e40fc72
MateusAranha <MatheusAranhaFranchini@gmail.com>
2023-07-04 09:29:04 -0300

    feat: added tests to the cartSlice selectors

--- courses/2023/confidently-testing-redux-application-with-jest-and-typescript/redux-shopping-cart-testing/src/features/cart/cartSlice.test.ts
+++ courses/2023/confidently-testing-redux-application-with-jest-and-typescript/redux-shopping-cart-testing/src/features/cart/cartSlice.test.ts
@@ -1,4 +1,6 @@
-import cartReducer, { CartState, addToCart, removeFromCart, updateQuantity } from './cartSlice';
+import cartReducer, { CartState, addToCart, removeFromCart, updateQuantity, getNumItems, getMemoizedNumItems, getTotalPrice} from './cartSlice';
+import type { RootState } from '../../app/store';
+import products from '../../../public/products.json';
 
 describe("cart reducer", () => {
     test("an empty action", () => {
@@ -60,6 +62,178 @@ describe("cart reducer", () => {
             checkoutState: "READY",
             errorMessage: "",
             items: { abc: 1, def: 5 }
+        });
+    });
+});
+
+describe("selectors", () => {
+    describe("getNumItens", () => {
+        it("should return 0 with no items", () => {
+            const cart: CartState = {
+                checkoutState: "READY",
+                errorMessage: "",
+                items: {}
+            };
+            const result = getNumItems({ cart } as RootState);
+            expect(result).toEqual(0);
+        });
+        it("should add up the total", () => {
+            const cart: CartState = {
+                checkoutState: "READY",
+                errorMessage: "",
+                items: { abc: 3, def: 3 }
+            };
+            const result = getNumItems({ cart } as RootState);
+            expect(result).toEqual(6);
+        });
+    });
+    describe("getMemoizedNumItems", () => {
+        it("should return 0 with no items", () => {
+            const cart: CartState = {
+                checkoutState: "READY",
+                errorMessage: "",
+                items: {}
+            };
+            const result = getMemoizedNumItems({ cart } as RootState);
+            expect(result).toEqual(0);
+        });
+        it("should add up the total", () => {
+            const cart: CartState = {
+                checkoutState: "READY",
+                errorMessage: "",
+                items: { abc: 3, def: 3 }
+            };
+            const result = getMemoizedNumItems({ cart } as RootState);
+            expect(result).toEqual(6);
+        });
+        it("should not compute again with the same state", () => {
+            const cart: CartState = {
+                checkoutState: "READY",
+                errorMessage: "",
+                items: { abc: 3, def: 3 }
+            };
+            getMemoizedNumItems.resetRecomputations();
+            getMemoizedNumItems({ cart } as RootState);
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+            getMemoizedNumItems({ cart } as RootState);
+            getMemoizedNumItems({ cart } as RootState);
+            getMemoizedNumItems({ cart } as RootState);
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+        });
+        it("should recompute with new state", () => {
+            const cart: CartState = {
+                checkoutState: "READY",
+                errorMessage: "",
+                items: { abc: 3, def: 3 }
+            };
+            getMemoizedNumItems.resetRecomputations();
+            getMemoizedNumItems({ cart } as RootState);
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+            cart.items = { abc: 2 };
+            getMemoizedNumItems({ cart } as RootState);
+            expect(getMemoizedNumItems.recomputations()).toEqual(2);
+        });
+    });
+    describe("getTotalPrice", () => {
+        it("should return 0 with an empty cart", () => {
+            const state: RootState = {
+                cart: { checkoutState: "READY", errorMessage: "", items: {} },
+                products: { products: {} }
+            }
+            const result = getTotalPrice(state);
+            expect(result).toEqual("0.00");
+        });
+        it("should add up the totals", () => {
+            const state: RootState = {
+                cart: { checkoutState: "READY", errorMessage: "", 
+                    items: {
+                        [products[0].id]: 3,
+                        [products[1].id]: 4,
+                    } 
+                },
+                products: { 
+                    products: {
+                        [products[0].id]: products[0],
+                        [products[1].id]: products[1]
+                    }
+                }
+            }
+            const result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+        });
+        it("should not compute again with the same state", () => {
+            const state: RootState = {
+                cart: { checkoutState: "READY", errorMessage: "", 
+                    items: {
+                        [products[0].id]: 3,
+                        [products[1].id]: 4,
+                    } 
+                },
+                products: { 
+                    products: {
+                        [products[0].id]: products[0],
+                        [products[1].id]: products[1]
+                    }
+                }
+            }
+            getTotalPrice.resetRecomputations();
+            const result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+            expect(getTotalPrice.recomputations()).toEqual(1);
+            getTotalPrice(state);
+            expect(getTotalPrice.recomputations()).toEqual(1);
+        });
+        it("should recompute with new products", () => {
+            const state: RootState = {
+                cart: { checkoutState: "READY", errorMessage: "", 
+                    items: {
+                        [products[0].id]: 3,
+                        [products[1].id]: 4,
+                    } 
+                },
+                products: { 
+                    products: {
+                        [products[0].id]: products[0],
+                        [products[1].id]: products[1]
+                    }
+                }
+            }
+            getTotalPrice.resetRecomputations();
+            let result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+            expect(getTotalPrice.recomputations()).toEqual(1);
+            state.products.products = {
+                [products[0].id]: products[0],
+                [products[1].id]: products[1],
+                [products[2].id]: products[2]
+            }
+            result = getTotalPrice({ ...state });
+            expect(result).toEqual("43.23");
+            expect(getTotalPrice.recomputations()).toEqual(2);
+        });
+        it("should recompute when cart changes", () => {
+            const state: RootState = {
+                cart: { checkoutState: "READY", errorMessage: "", 
+                    items: {
+                        [products[0].id]: 3,
+                        [products[1].id]: 4,
+                    } 
+                },
+                products: { 
+                    products: {
+                        [products[0].id]: products[0],
+                        [products[1].id]: products[1]
+                    }
+                }
+            }
+            getTotalPrice.resetRecomputations();
+            let result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+            expect(getTotalPrice.recomputations()).toEqual(1);
+            state.cart.items = {};
+            result = getTotalPrice({ ...state });
+            expect(result).toEqual("0.00");
+            expect(getTotalPrice.recomputations()).toEqual(2);
         });
     });
 });
